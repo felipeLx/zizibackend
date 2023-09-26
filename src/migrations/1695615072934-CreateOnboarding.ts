1c6862bf1fcf230ac7cc646a7f0a004aeb0259aa
felipeLx <felipealvesfinanceiro@gmail.com>
2023-09-25 23:55:40 -0300

    :bug: external image url8

---
+++ src/migrations/1695615072934-CreateOnboarding.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateOnboarding1695615072934 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
