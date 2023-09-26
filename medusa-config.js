4b67d6a8ca375561644d05bc6722799fc95a4c09
felipeLx <felipealvesfinanceiro@gmail.com>
2023-09-26 00:50:56 -0300

    :bug: external image url1

--- medusa-config.js
+++ medusa-config.js
@@ -29,7 +29,7 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  process.env.DATABASE_URL;
 
 const CACHE_REDIS_URL = process.env.CACHE_REDIS_URL || "redis://localhost:6379";
 
